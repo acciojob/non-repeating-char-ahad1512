ab020337b357b89988cdda019e9a8617a526f420
ahad1512 <139266271+ahad1512@users.noreply.github.com>
2024-07-29 01:44:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let ct =1;
+	for (let i = 0; i < str.length; i++) {
+		let ch = str.charAt(i);
+		if(ch === str.charAt(i+1)){
+			ct++;
+		}
+		else{
+			if(ct===1){
+				return ch;
+			}
+			ch = str.charAt(i+1);
+			ct=1;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
